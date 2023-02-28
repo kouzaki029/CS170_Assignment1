2dfe7f1aaab548faa1cf79477a15749560b9a336
kouzaki029 <122118874+kouzaki029@users.noreply.github.com>
2023-02-27 21:12:50 -0500

    noticed typos

--- problem1.cpp
+++ problem1.cpp
@@ -4,23 +4,27 @@ using std::cout, std::cin, std::numeric_limits, std::streamsize;
 
 int main()
 {
-    int day = 0;
-    int month = 0;
-    int year = 0;
+    int firstDAY = 0;
+    int firstMONTH = 0;
+    int firstYEAR = 0;
+    int secondDAY = 0;
+    int secondMONTH = 0;
+    int secondYEAR = 0;
     int errorCount = 0;
     int userEntry = 0;
     bool done = false;
 
     while(!done)
     {
-        if(day == 0)
+        //retrieving first Date
+        if(firstDAY == 0)
         {
             if(errorCount == 3)
             {
-                cout << "\nYou entered day and/or month incorrectly three times.\nProgram closing...\n";
+                cout << "\nYou entered the day and/or month incorrectly three times.\nProgram closing...\n";
                 break;
             }
-            cout << "Please enter a number for day (e.g., 02): ";
+            cout << "Please enter a number for firstDAY (e.g., 02): ";
             cin >> userEntry;
             if (cin.fail() || userEntry == 0)
             {
@@ -31,18 +35,18 @@ int main()
             }
             else
             {
-                day = userEntry;
-                //cout << "day = " << day << "\n";
+                firstDAY = userEntry;
+                //cout << "firstDAY = " << firstDAY << "\n";
             }
         }
-        else if(month == 0)
+        else if(firstMONTH == 0)
         {
             if(errorCount == 3)
             {
-                cout << "\nYou entered day and/or month incorrectly three times.\nProgram closing...\n";
+                cout << "\nYou entered the day and/or month incorrectly three times.\nProgram closing...\n";
                 break;
             }
-            cout << "Please enter a number for month (e.g., 03): ";
+            cout << "Please enter a number for firstMONTH (e.g., 03): ";
             cin >> userEntry;
             if (cin.fail() || userEntry == 0)
             {
@@ -53,13 +57,13 @@ int main()
             }
             else
             {
-                month = userEntry;
-                //cout << "month = " << month << "\n";
+                firstMONTH = userEntry;
+                //cout << "firstMONTH = " << firstMONTH << "\n";
             }
         }
-        else if(year == 0)
+        else if(firstYEAR == 0)
         {
-            cout << "Please enter a number for year (e.g., 03): ";
+            cout << "Please enter a number for firstYEAR (e.g., 03): ";
             cin >> userEntry;
             if (cin.fail() || userEntry == 0)
             {
@@ -70,16 +74,80 @@ int main()
             }
             else
             {
-                year = userEntry;
-                //cout << "year = " << year << "\n";
+                firstYEAR = userEntry;
+                //cout << "firstYEAR = " << firstYEAR << "\n";
             }
         }
-        else if(day > 0 && month > 0 && year > 0)
+
+        //retrieving second Date
+        else if(secondDAY == 0)
         {
-            cout << "good job!\n";
+            if(errorCount == 3)
+            {
+                cout << "\nYou entered the day and/or month incorrectly three times.\nProgram closing...\n";
+                break;
+            }
+            cout << "Please enter a number for secondDAY (e.g., 02): ";
+            cin >> userEntry;
+            if (cin.fail() || userEntry == 0)
+            {
+                ++errorCount;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid entry.\n";
+            }
+            else
+            {
+                secondDAY = userEntry;
+                //cout << "secondDAY = " << secondDAY << "\n";
+            }
         }
-        else
-            done = true;
+        else if(secondMONTH == 0)
+        {
+            if(errorCount == 3)
+            {
+                cout << "\nYou entered the day and/or month incorrectly three times.\nProgram closing...\n";
+                break;
+            }
+            cout << "Please enter a number for secondMONTH (e.g., 03): ";
+            cin >> userEntry;
+            if (cin.fail() || userEntry == 0)
+            {
+                ++errorCount;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid entry.\n";
+            }
+            else
+            {
+                secondMONTH = userEntry;
+                //cout << "secondMONTH = " << secondMONTH << "\n";
+            }
+        }
+        else if(secondYEAR == 0)
+        {
+            cout << "Please enter a number for secondYEAR (e.g., 03): ";
+            cin >> userEntry;
+            if (cin.fail() || userEntry == 0)
+            {
+                ++errorCount;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid entry.\n";
+            }
+            else
+            {
+                secondYEAR = userEntry;
+                //cout << "secondYEAR = " << secondYEAR << "\n";
+            }
+        }
+
+        //calculate the difference in days
+        else if(firstDAY > 0 && firstMONTH > 0 && firstYEAR > 0 && secondDAY > 0 && secondMONTH > 0 && secondYEAR > 0)
+        {
+
+        }
+
     }
 
     return 0;
