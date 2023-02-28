64e359ec02f8662e684ec9bca00451ef5a26cd8c
kouzaki029 <122118874+kouzaki029@users.noreply.github.com>
2023-02-28 16:51:30 -0500

    Update differenceInDays.cpp

--- differenceInDays.cpp
+++ differenceInDays.cpp
@@ -1,21 +1,19 @@
-//if the days and month are the same between two dates
-
 #include <iostream>
 
 int main()
 {
-    int d1 = 01;
-    int m1 = 23;
+    int d1 = 23;
+    int m1 = 10;
     int y1 = 2020;
-    int d2 = 01;
-    int m2 = 23;
+    int d2 = 23;
+    int m2 = 01;
     int y2 = 1899;
 
     // 01/23/2020 - 01/23/1899 = 44194 days
     // 01/23/2021 - 01/23/1899 = 44560 days
-    // 02/23/2020 - 01/23/1899 = 44225 days (44194 + 8 + 23)
-    // 03/23/2020 - 01/23/1899 = 23 + 29 + 8 + 44194
-    // 04/23/2020 - 01/23/1899 = 23 + 31 + 29 + 8 + 44194
+    // 02/23/2020 - 01/23/1899 =                23 + 8 + 44194
+    // 03/23/2020 - 01/23/1899 =           23 + 29 + 8 + 44194
+    // 04/23/2020 - 01/23/1899 =      23 + 31 + 29 + 8 + 44194
     // 05/23/2020 - 01/23/1899 = 23 + 30 + 31 + 29 + 8 + 44194
     //step 1: subtract the difference between days (e.g., 1/23 - 1/23 = 0)
     //step 2: determine if (YEAR - 1) is a leap year, add 366 to 0.
@@ -23,9 +21,10 @@ int main()
     //step 3: keep repeating step 2 until YEAR = secondYear
 
     int remainingDay = 0;
+    int leapDays = 0;
     int calculatingDays = 0;
 
-        //days are the same, but the month and year are different
+    //days are the same, but the month and year are different
     if(m1 == 2)
     {
         remainingDay = ( (d1) + (31-d1) );
@@ -70,21 +69,24 @@ int main()
     {
         remainingDay = ( (d1) + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31 + 29 + (31-d1) );
     }
-    
-    
-    while(y1 != y2)
+
+    //days and months are the same, but the year is different
+    while(y1 != y2 )
     {
         --y1;
         //std::cout << "calculatingDays = " << calculatingDays << " calculatingDays\ty1 = " << y1 << "\n";
         if(y1 % 4 == 0 && y1 % 100 == 0 && y1 % 400 == 0)
-            calculatingDays += 366;
+            leapDays += 366;
         else if(y1 % 4 == 0 && y1 % 100 != 0 && y1 % 400 != 0)
-            calculatingDays += 366;
+            leapDays += 366;
         else
-            calculatingDays += 365;
+            leapDays += 365;
     }
+    calculatingDays = remainingDay + leapDays;
 
-    std::cout << "\tcalculatingDays = " << calculatingDays << " calculatingDays\n";
+    std::cout << "\tremainingDay = " << remainingDay << "\n";
+    std::cout << "\tleapDays = " << leapDays << "\n";
+    std::cout << "\tcalculatingDays = " << calculatingDays << "\n";
 
     return 0;
 }
