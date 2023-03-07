db19b43b8ec29fc92f5825fe60d1d5f0d429559c
kouzaki029 <122118874+kouzaki029@users.noreply.github.com>
2023-03-06 20:54:46 -0500

    Update problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -2,217 +2,358 @@
     program does not account the Julian calendar; in other words anything before 1582 will be off because it was a part of the Julian calendar and not the Gregorian
 */
 #include <iostream>
+#include<limits>
+using std::cout, std::cin, std::numeric_limits, std::streamsize;
 
 int main()
 {
-    /*int dayRange = 28 - 1 + 1;
-    int monthRange = 12 - 1 + 1;
-    int yearRange = 900 - 3000 + 1;*/
-
-    /*int d1 = rand() % dayRange + 1;
-    int m1 = rand() % monthRange + 1;
-    int y1 = rand() % yearRange + 1;
-    int d2 = rand() % dayRange + 1;
-    int m2 = rand() % monthRange + 1;
-    int y2 = rand() % yearRange + 1;
-
-
-    while (y2 > y1)
-    {
-        y2 = rand() % yearRange + 1;
-    }*/
-    int monthDifference = 0;
-    int yearDifference = 0;
-    int calculatingDays = 0;
-
+    int errorCount = 0;
+    int userEntry = 0;
+    bool done = false;
     //m1, d1, and y1 are for the newer date
-    int m1 = 11;
-    int d1 = 23;
-    int y1 = 2020;
+    int m1 = 0;
+    int d1 = 0;
+    int y1 = 0;
     //m2, d2, and y2 are for the older date
-    int m2 = 1;
-    int d2 = 16;
-    int y2 = 1899;
+    int m2 = 0;
+    int d2 = 0;
+    int y2 = 0;
 
-    std::cout << "newerDate:\t" << m1 << "/" << d1 << "/" << y1 << "\n";
-    std::cout << "olderDate:\t" << m2 << "/" << d2 << "/" << y2 << "\n";
-
-    /* 01/23/2020 - 01/23/1899 = 44194 days
-     01/23/2021 - 01/23/1899 = 44560 days
-     02/23/2020 - 01/23/1899 =                23 + 8 + 44194
-     03/23/2020 - 01/23/1899 =           23 + 29 + 8 + 44194
-     04/23/2020 - 01/23/1899 =      23 + 31 + 29 + 8 + 44194
-     05/23/2020 - 01/23/1899 = 23 + 30 + 31 + 29 + 8 + 44194
-     01/24/2020 - 01/23/1899 =
-    step 1: subtract the difference between days (e.g., 1/23 - 1/23 = 0)
-    step 2: determine if (YEAR - 1) is a leap year, add 366 to 0.
-                      if (YEAR - 1) is NOT a leap year, add 365 to 0.
-    step 3: keep repeating step 2 until YEAR = secondYear*/
-
-    //the day, month and year are different
-    int dayDifference = d1 - d2;
-
-    //days are the same, but the month and year are different
-    if (m1 > m2 || m1 == m2) //e.g., newDate = 11/23/2020 and olderDate = 1/23/2020
+    while (!done)
     {
-        while (m1 > m2)
+        //retrieving first Date
+        if (d1 == 0)
         {
-            --m1;
-            if (m1 == 1)
+            if (errorCount == 3)
             {
-                monthDifference += 31;
+                cout << "\nYou entered the day and/or month incorrectly three times.\nProgram closing...\n";
+                break;
             }
-            if (m1 == 2)
+            cout << "Please enter a number for d1 (e.g., 02): ";
+            cin >> userEntry;
+            if (cin.fail() || userEntry == 0 || userEntry > 31)
             {
-                if (y1 % 4 == 0 && y1 % 100 == 0 && y1 % 400 == 0)
-                {
-                    monthDifference += 29;
-                }
-                else if (y1 % 4 == 0 && y1 % 100 != 0 && y1 % 400 != 0)
-                {
-                    monthDifference += 29;
-                }
-                else
-                {
-                    monthDifference += 28;
-                }
+                ++errorCount;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid entry.\n";
             }
-            if (m1 == 3)
+            else
             {
-                monthDifference += 31;
+                d1 = userEntry;
+                //cout << "d1 = " << d1 << "\n";
             }
-            if (m1 == 4)
+        }
+        else if (m1 == 0)
+        {
+            if (errorCount == 3)
             {
-                monthDifference += 30;
+                cout << "\nYou entered the day and/or month incorrectly three times.\nProgram closing...\n";
+                break;
             }
-            if (m1 == 5)
+            cout << "Please enter a number for m1 (e.g., 03): ";
+            cin >> userEntry;
+            if (cin.fail() || userEntry == 0 || userEntry > 12)
             {
-                monthDifference += 31;
+                ++errorCount;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid entry.\n";
             }
-            if (m1 == 6)
+
+            //validating the correct number of days per month
+            if (d1 == 31 && (userEntry == 2 || userEntry == 4 || userEntry == 6 || userEntry == 9 || userEntry == 11))
             {
-                monthDifference += 30;
+                ++errorCount;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid entry. There are not 31 days in ";
+                if (userEntry == 2)
+                    cout << "Feb.\n";
+                else if (userEntry == 4)
+                    cout << "Apr.\n";
+                else if (userEntry == 6)
+                    cout << "Jun.\n";
+                else if (userEntry == 9)
+                    cout << "Aug.\n";
+                else if (userEntry == 11)
+                    cout << "Nov.\n";
             }
-            if (m1 == 7)
+            else if (d1 == 30 && userEntry == 2)
             {
-                monthDifference += 31;
+                ++errorCount;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid entry. There are not 30 days in Feb.\n";
             }
-            if (m1 == 8)
+
+            else
             {
-                monthDifference += 31;
+                m1 = userEntry;
+                //cout << "d1 = " << d1 << "\tm1 = " << m1 << "\n";
+                //cout << "m1 = " << m1 << "\n";
             }
-            if (m1 == 9)
+        }
+        else if (y1 == 0)
+        {
+            cout << "Please enter a number for y1 (e.g., 03): ";
+            cin >> userEntry;
+            if (cin.fail() || userEntry == 0)
             {
-                monthDifference += 30;
+                ++errorCount;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid entry.\n";
             }
-            if (m1 == 10)
+            else
             {
-                monthDifference += 31;
+                y1 = userEntry;
+                //cout << "d1 = " << d1 << "\tm1 = " << m1 << "\ty1 = " << y1 << "\n";
+                //cout << "y1 = " << y1 << "\n";
             }
-            if (m1 == 11)
+        }
+
+        //retrieving second Date
+        else if (d2 == 0)
+        {
+            if (errorCount == 3)
             {
-                monthDifference += 30;
+                cout << "\nYou entered the day and/or month incorrectly three times.\nProgram closing...\n";
+                break;
             }
-            if (m1 == 12)
+            cout << "Please enter a number for d2 (e.g., 02): ";
+            cin >> userEntry;
+            if (cin.fail() || userEntry == 0 || userEntry > 31)
             {
-                monthDifference += 31;
+                ++errorCount;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid entry.\n";
+            }
+            else
+            {
+                d2 = userEntry;
+                //cout << "d2 = " << d2 << "\n";
+            }
+        }
+        else if (m2 == 0)
+        {
+            if (errorCount == 3)
+            {
+                cout << "\nYou entered the day and/or month incorrectly three times.\nProgram closing...\n";
+                break;
+            }
+            cout << "Please enter a number for m2 (e.g., 03): ";
+            cin >> userEntry;
+            if (cin.fail() || userEntry == 0 || userEntry > 12)
+            {
+                ++errorCount;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid entry.\n";
+            }
+            else
+            {
+                m2 = userEntry;
+                //cout << "m2 = " << m2 << "\n";
+            }
+        }
+        else if (y2 == 0)
+        {
+            cout << "Please enter a number for y2 (e.g., 03): ";
+            cin >> userEntry;
+            if (cin.fail() || userEntry == 0)
+            {
+                ++errorCount;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid entry.\n";
+            }
+            else
+            {
+                y2 = userEntry;
+                //cout << "y2 = " << y2 << "\n";
+                done = true;
             }
         }
     }
-    else //e.g., newerDate = 1/23/2020 and olderDate = 8/23/2019
+
+
+    while (errorCount < 3)
     {
-    --y1;
-        while (m1 != m2)
+        std::cout << "newerDate:\t" << m1 << "/" << d1 << "/" << y1 << "\n";
+        std::cout << "olderDate:\t" << m2 << "/" << d2 << "/" << y2 << "\n";
+
+        //the day, month and year are different
+        int dayDifference = d1 - d2;
+
+        int monthDifference = 0;
+        int yearDifference = 0;
+        int calculatingDays = 0;
+
+        //days are the same, but the month and year are different
+        if (m1 > m2 || m1 == m2) //e.g., newDate = 11/23/2020 and olderDate = 1/23/2020
         {
-            --m1;
-            if (m1 == 1)
+            while (m1 > m2)
             {
-                monthDifference += 31;
-                m1 = 12;
-            }
-            if (m1 == 2)
-            {
-                if (y1 % 4 == 0 && y1 % 100 == 0 && y1 % 400 == 0)
+                --m1;
+                if (m1 == 1)
                 {
-                    monthDifference += 29;
+                    monthDifference += 31;
                 }
-                else if (y1 % 4 == 0 && y1 % 100 != 0 && y1 % 400 != 0)
+                if (m1 == 2)
                 {
-                    monthDifference += 29;
+                    if (y1 % 4 == 0 && y1 % 100 == 0 && y1 % 400 == 0)
+                    {
+                        monthDifference += 29;
+                    }
+                    else if (y1 % 4 == 0 && y1 % 100 != 0 && y1 % 400 != 0)
+                    {
+                        monthDifference += 29;
+                    }
+                    else
+                    {
+                        monthDifference += 28;
+                    }
                 }
-                else
+                if (m1 == 3)
                 {
-                    monthDifference += 28;
+                    monthDifference += 31;
+                }
+                if (m1 == 4)
+                {
+                    monthDifference += 30;
+                }
+                if (m1 == 5)
+                {
+                    monthDifference += 31;
+                }
+                if (m1 == 6)
+                {
+                    monthDifference += 30;
+                }
+                if (m1 == 7)
+                {
+                    monthDifference += 31;
+                }
+                if (m1 == 8)
+                {
+                    monthDifference += 31;
+                }
+                if (m1 == 9)
+                {
+                    monthDifference += 30;
+                }
+                if (m1 == 10)
+                {
+                    monthDifference += 31;
+                }
+                if (m1 == 11)
+                {
+                    monthDifference += 30;
+                }
+                if (m1 == 12)
+                {
+                    monthDifference += 31;
                 }
             }
-            if (m1 == 3)
+        }
+        else //e.g., newerDate = 1/23/2020 and olderDate = 8/23/2019
+        {
+            --y1;
+            while (m1 != m2)
             {
-                monthDifference += 31;
+                --m1;
+                if (m1 == 1)
+                {
+                    monthDifference += 31;
+                    m1 = 12;
+                }
+                if (m1 == 2)
+                {
+                    if (y1 % 4 == 0 && y1 % 100 == 0 && y1 % 400 == 0)
+                    {
+                        monthDifference += 29;
+                    }
+                    else if (y1 % 4 == 0 && y1 % 100 != 0 && y1 % 400 != 0)
+                    {
+                        monthDifference += 29;
+                    }
+                    else
+                    {
+                        monthDifference += 28;
+                    }
+                }
+                if (m1 == 3)
+                {
+                    monthDifference += 31;
+                }
+                if (m1 == 4)
+                {
+                    monthDifference += 30;
+                }
+                if (m1 == 5)
+                {
+                    monthDifference += 31;
+                }
+                if (m1 == 6)
+                {
+                    monthDifference += 30;
+                }
+                if (m1 == 7)
+                {
+                    monthDifference += 31;
+                }
+                if (m1 == 8)
+                {
+                    monthDifference += 31;
+                }
+                if (m1 == 9)
+                {
+                    monthDifference += 30;
+                }
+                if (m1 == 10)
+                {
+                    monthDifference += 31;
+                }
+                if (m1 == 11)
+                {
+                    monthDifference += 30;
+                }
+                if (m1 == 12)
+                {
+                    monthDifference += 31;
+                }
             }
-            if (m1 == 4)
+        }
+
+
+        //days and months are the same, but the year is different
+        while (y1 != y2)
+        {
+            /*DEBUG PURPOSES: std::cout << "yearDifference = " << yearDifference << " yearDifference\ty1 = " << y1 << "\n";*/
+            if (y1 % 4 == 0 && y1 % 100 == 0 && y1 % 400 == 0)
             {
-                monthDifference += 30;
+                yearDifference += 366;
             }
-            if (m1 == 5)
+            else if (y1 % 4 == 0 && y1 % 100 != 0 && y1 % 400 != 0)
             {
-                monthDifference += 31;
+                yearDifference += 366;
             }
-            if (m1 == 6)
+            else
             {
-                monthDifference += 30;
+                yearDifference += 365;
             }
-            if (m1 == 7)
-            {
-                monthDifference += 31;
-            }
-            if (m1 == 8)
-            {
-                monthDifference += 31;
-            }
-            if (m1 == 9)
-            {
-                monthDifference += 30;
-            }
-            if (m1 == 10)
-            {
-                monthDifference += 31;
-            }
-            if (m1 == 11)
-            {
-                monthDifference += 30;
-            }
-            if (m1 == 12)
-            {
-                monthDifference += 31;
-            }
-        } 
+            --y1;
+        }
+        calculatingDays = dayDifference + monthDifference + yearDifference;
+
+        std::cout << "\tdayDifference = " << dayDifference << "\n";
+        std::cout << "\tmonthDifference = " << monthDifference << "\n";
+        std::cout << "\tyearDifference = " << yearDifference << "\n";
+        std::cout << "\tcalculatingDays = " << calculatingDays << "\n";
+        errorCount = 3;
     }
-    
 
-    //days and months are the same, but the year is different
-    while (y1 != y2)
-    {        
-        /*DEBUG PURPOSES: std::cout << "yearDifference = " << yearDifference << " yearDifference\ty1 = " << y1 << "\n";*/
-        if (y1 % 4 == 0 && y1 % 100 == 0 && y1 % 400 == 0)
-        {
-            yearDifference += 366;
-        }
-        else if (y1 % 4 == 0 && y1 % 100 != 0 && y1 % 400 != 0)
-        {
-            yearDifference += 366;
-        }
-        else
-        {
-            yearDifference += 365;
-        }
-        --y1;
-    }
-    calculatingDays = dayDifference + monthDifference + yearDifference;
-
-    std::cout << "\tdayDifference = " << dayDifference << "\n";
-    std::cout << "\tmonthDifference = " << monthDifference << "\n";
-    std::cout << "\tyearDifference = " << yearDifference << "\n";
-    std::cout << "\tcalculatingDays = " << calculatingDays << "\n";
-
-    return 0;
+        return 0;
 }
